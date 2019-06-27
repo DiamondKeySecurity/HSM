751a662562009cfd80f878e9a5bcec3398b10f93
Dominque Douglas <douglas@dkey.org>
2019-06-27 16:31:27 -0500

    set password script updates

--- hsm_software/sw/console/scripts/password.py
+++ hsm_software/sw/console/scripts/password.py
@@ -20,22 +20,28 @@ from script import ScriptModule, script_node, ValueType
 from hsm_tools.cryptech_port import DKS_HALUser
 
 class PasswordScriptModule(ScriptModule):
-    def __init__(self, cty_direct_call, set_hide_input, cty_connection, user):
+    def __init__(self, cty_direct_call, set_hide_input, cty_connection, user, must_set = False):
         self.cty_direct_call = cty_direct_call
         self.set_hide_input = set_hide_input
         self.cty_connection = cty_connection
         self.user = user
-        super(PasswordScriptModule, self).__init__(node_list = [
-                        script_node('continue',
-                                    "Would you like to set the '%s' PIN? (y/n) "%DKS_HALUser.to_name(user),
-                                    ValueType.YesNo, callback=self.continuePromptCallback),
-                        script_node('password',
-                                    "Enter the new '%s' PIN: "%DKS_HALUser.to_name(user),
-                                    ValueType.AnyString, callback=None),
-                        script_node('confirm_password',
-                                    "Confirm the new '%s' PIN: "%DKS_HALUser.to_name(user),
-                                    ValueType.AnyString, callback=self.confirmPassword)
-                        ])
+
+        node_list = []
+
+        if (not must_set):
+            node_list.append(script_node('continue',
+                                         "Would you like to set the '%s' PIN? (y/n) "%DKS_HALUser.to_name(user),
+                                         ValueType.YesNo, callback=self.continuePromptCallback))
+
+        node_list.append(script_node('password',
+                                     "Enter the new '%s' PIN: "%DKS_HALUser.to_name(user),
+                                     ValueType.AnyString, callback=None))
+        node_list.append(script_node('confirm_password',
+                                     "Confirm the new '%s' PIN: "%DKS_HALUser.to_name(user),
+                                     ValueType.AnyString, callback=self.confirmPassword))
+
+
+        super(PasswordScriptModule, self).__init__(node_list = node_list)
 
     def continuePromptCallback(self, response):
         if(response == True):
@@ -45,16 +51,24 @@ class PasswordScriptModule(ScriptModule):
         return None
 
     def confirmPassword(self, response):
+        accepted = False
+
         if (' ' in self.results['password'] or 
            ('\t' in self.results['password'])):
             self.cty_direct_call('Sorry, but the PIN cannot contain any white space characters. PIN not accepted.')
         elif (self.results['password'] != self.results['confirm_password']):
             self.cty_direct_call('Sorry, but the PIN does not match. PIN not accepted.')
         else:
+            accepted = True
             password = self.results['password']
 
             self.cty_connection.setPassword(DKS_HALUser.to_name(self.user), password)
 
         self.set_hide_input(False)
 
-        return None
+        if (accepted == False):
+            # go back to the first node
+            self.current = 0
+            return self
+        else:
+            return None
