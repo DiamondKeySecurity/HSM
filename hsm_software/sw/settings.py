5476565c9a94a02a6df1d8646e59b4312ccb25b3
Dominque Douglas <douglas@dkey.org>
2019-07-08 15:01:58 -0500

    missing from merge

--- hsm_software/sw/settings.py
+++ hsm_software/sw/settings.py
@@ -21,11 +21,7 @@ import threading
 
 from enum import Enum
 
-<<<<<<< HEAD
-HSM_SOFTWARE_VERSION = '19.07.03.field03'
-=======
-HSM_SOFTWARE_VERSION = '19.07.08.surfnet12'
->>>>>>> devSurfNet
+HSM_SOFTWARE_VERSION = '19.07.08.field04'
 
 # this is the version of the firmware that's built into the current release
 BUILTIN_FIRMWARE_VERSION = '2019-06-26-tamper'
