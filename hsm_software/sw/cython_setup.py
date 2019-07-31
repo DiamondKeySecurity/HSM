d824f35bd0618879f35729a4d5ca7b6d50b5b403
Dominque Douglas <douglas@dkey.org>
2019-07-31 16:10:33 -0500

    short prep for Cython

---
+++ hsm_software/sw/cython_setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Copyright (c) 2019  Diamond Key Security, NFP
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License only.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, If not, see <https://www.gnu.org/licenses/>.
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(name='RPC Handler',
+      ext_modules=cythonize("*.pyx"))
