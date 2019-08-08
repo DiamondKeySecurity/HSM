c57f9a9ce873a3cb06ac526b9c2eb3758377ac83
Dominque Douglas <douglas@dkey.org>
2019-08-08 14:16:28 -0500

    unsaved changes

--- hsm_software/sw/hsm_data/c_code/_xdr.h
+++ hsm_software/sw/hsm_data/c_code/_xdr.h
@@ -1,1 +1,24 @@
+// Copyright(c) 2019  Diamond Key Security, NFP
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+// of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, If not, see <https://www.gnu.org/licenses/>.
+
+// PURE C++ XDR Implementation
+
 #define HAL_RPC_MAX_PKT_SIZE    16384
+
+class xdr_packer
+{
+    public:
+        
+};
