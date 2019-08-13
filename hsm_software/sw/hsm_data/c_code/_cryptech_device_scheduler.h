26116a8a74ac92b22bc2c9a8d91380ad53d77c90
Dominque Douglas <douglas@dkey.org>
2019-08-13 10:08:38 -0500

    progress

--- hsm_software/sw/hsm_data/c_code/_cryptech_device_scheduler.h
+++ hsm_software/sw/hsm_data/c_code/_cryptech_device_scheduler.h
@@ -18,15 +18,39 @@
 
 #include "libhal/rpc_packet.h"
 #include "libhal/rpc_stream.h"
+#include "libhal/safe_queue.h"
+
+namespace diamond_hsm
+{
 
 class cyptech_device_scheduler
+// creates a single queue that multiple worker threads can pull from.
+// Each thread logs into the HSM and runs commands off of it.
+// Authentication on RPC is handled by the HSM single-board computer.
+// This setup overcomes the CrypTech Alpha's maximum connected clients,
+// because all connections share clients.
 {
+    private:
+        struct QueuedData
+        {
+            
+            libhal::rpc_packet *opacket;
+
+            int worker_thread_index;
+        };
+
     public:
-        cyptech_device_scheduler(const char *dev);
+        cyptech_device_scheduler();
+
         ~cyptech_device_scheduler();
+
+        hal_error_t create(const char * const device, const uint32_t speed, const int num_workers,
+                           const hal_user_t user, const char *pin);
 
     private:
 
-}
 
+};
+
+}
 #endif
