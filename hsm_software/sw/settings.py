9ac3a10b73e8910196ee80df411cb30f613b638b
Dominque Douglas <douglas@dkey.org>
2019-08-29 09:54:08 -0500

    19.08.29.cython36

--- hsm_software/sw/settings.py
+++ hsm_software/sw/settings.py
@@ -22,7 +22,7 @@ from cryptech.cty_connection import HSM_BINARY_FILE
 
 from enum import Enum
 
-HSM_SOFTWARE_VERSION = '19.08.27.cython35'
+HSM_SOFTWARE_VERSION = '19.08.29.cython36'
 
 # this is the version of the firmware that's built into the current release
 BUILTIN_FIRMWARE_VERSION = HSM_BINARY_FILE
